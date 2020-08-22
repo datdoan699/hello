7c6a2e26fe13ad04028494ac398516c658d9ee75
Dat Doan <ddoan4@huskers.unl.edu>
2020-08-22 14:55:04 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Dat Doan
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("hello world!\n");
+ }
